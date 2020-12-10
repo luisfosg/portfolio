6edca03cb18e4ca39f785e00007009a45aec8576
LuisFOsG <luisfernandogarciaosorio@gmail.com>
2020-12-10 15:57:55 -0500

    Ajustando Espacio de Trabajo

---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        My porfolio
+      </header>
+    </div>
+  );
+}
+
+export default App;
