167a4ee8039ca7631e3e8d8b3bf70a754c94d28d
LuisFOsG <luisfosgdev@gmail.com>
2021-11-07 16:09:39 -0500

    fix: add User-Agent

--- src/pages/api/blog/index.ts
+++ src/pages/api/blog/index.ts
@@ -1,22 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  data: string
-}
-
 const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) => {
   const url = 'https://dev.to/api/articles/me'
   const resData = await fetch(url, {
     headers: {
       'Content-Type': 'application/json',
-      'api-key': `${process.env.API_KEY_DEVTO}`
+      'api-key': `${process.env.API_KEY_DEVTO}`,
+      'User-Agent': '*'
     }
   })
   const data = await resData.json()
 
+  if (!data) return res.status(404).json({ data: 'Not found' })
   res.status(200).json(data)
 }
 
