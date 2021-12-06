1470d0d7e665b730065fefa649c4662cb8bc16d1
LuisFOsG <luisfosgdev@gmail.com>
2021-12-05 19:58:25 -0500

    fix: delete icons

--- src/components/Icons/left.tsx
+++
@@ -1,9 +0,0 @@
-function LeftIcon (props: any) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} {...props}>
-      <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167L16.67 24 4.5 12.004z" />
-    </svg>
-  )
-}
-
-export default LeftIcon
