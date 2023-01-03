a0644b5e4be92372f95aa8c915f0f9f6585db083
LuisFOsG <luisfosgdev@gmail.com>
2023-01-03 11:59:33 -0500

    feat: header, footer and image blog

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  darkMode: 'class',
+  mode: 'jit',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
 }
