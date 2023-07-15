6e553055ba70e1470078742b515a77a5f235001b
luisfosg <luisfosgdev@gmail.com>
2023-07-14 23:06:18 -0500

    feat: colocando icono por defecto antes de mostrar el seleccionado

--- src/components/Darkmode.tsx
+++ src/components/Darkmode.tsx
@@ -1,21 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { toggleDark } from '../store/darkmode'
 
 import Button from '@common/ui/button'
 
 import DarkIcon from '../icons/DarkIcon'
+import LoadThemeIcon from '../icons/LoadTheme'
 import LightIcon from '../icons/LightIcon'
 
 const DarkMode = () => {
+  const [isMounted, setIsMounted] = useState(false)
   const [darkToggle, setDarkToggle] = useState<boolean>(() => {
     const theme = localStorage.getItem('theme') || 'light'
 
     return theme === 'dark'
   })
 
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
   const handlerTheme = (): void => {
     setDarkToggle(prev => !prev)
     toggleDark()
+  }
+
+  if (!isMounted) {
+    return (
+      <li >
+        <Button onclick={() => {}}>
+          <LoadThemeIcon />
+        </Button>
+      </li>
+    )
   }
 
   return (
