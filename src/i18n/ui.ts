5b8f93d66dd2eab6df6f6b16832c0c3c95787419
luisfosg <luisfosgdev@gmail.com>
2023-07-15 18:05:13 -0500

    feat: multilenguaje "nativo" en astro

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+  },
+  es: {
+    'nav.home': 'Inicio',
+    'nav.about': 'Acerca de',
+  },
+} as const;
