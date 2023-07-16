7c8bed5087441fabc58fef3d65909c782e338682
luisfosg <luisfosgdev@gmail.com>
2023-07-16 12:55:50 -0500

    fix: importando path desde un archivo en comun

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,4 @@
-import { ui, defaultLang } from './ui';
+import { ui, defaultLang, languages } from './ui';
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
@@ -10,4 +10,11 @@ export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof typeof ui[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
   }
+}
+
+export const getStaticPath = async () => {
+  const langs = Object.keys(languages)
+  const paths = langs.map(lang  => ({ params: { lang }}))
+
+  return paths;
 }
