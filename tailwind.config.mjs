defec41fd70e483c65675e28c523479af6224fc2
luisfosg <luisfosgdev@gmail.com>
2023-12-02 23:50:45 -0500

    feat: sección de profile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+    addDynamicIconSelectors()
+  ],
 }
