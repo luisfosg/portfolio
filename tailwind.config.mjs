5ff46eac9e58410c01ad2d544c6a7a26b6162a67
luisfosg <luisfosgdev@gmail.com>
2023-12-08 22:29:42 -0500

    feat: diseño básico del perfil

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,15 @@
 const { addDynamicIconSelectors } = require('@iconify/tailwind');
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    "./node_modules/flowbite/**/*.js"
+  ],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-    addDynamicIconSelectors()
+    addDynamicIconSelectors(),
+    require('flowbite/plugin')
   ],
 }
