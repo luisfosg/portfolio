5a34317c5758421cd6ebbb6427bca2a2ef0f3298
luisfosg <luisfosgdev@gmail.com>
2023-12-09 17:22:35 -0500

    feat: integrando react y animación en index

---
+++ src/components/react/Typing.tsx
@@ -0,0 +1,22 @@
+import { TypeAnimation } from 'react-type-animation';
+
+type Props = {
+  items: (string | number)[];
+  message: string;
+  classStyle: string;
+}
+
+const Typing = ({ items, message, classStyle }: Props) => {
+  return (
+    <span className={classStyle}>
+      <TypeAnimation
+        sequence={items}
+        wrapper="span"
+        speed={10}
+        repeat={Infinity}
+      /> <span>{message}</span>
+    </span>
+  )
+}
+
+export default Typing
