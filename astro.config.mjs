ed15aa50b59634b8881d03ec7556324a3047295e
luisfosg <luisfosgdev@gmail.com>
2023-12-09 11:40:33 -0500

    chore: actualizando astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), sentry(), spotlightjs()]
 });
