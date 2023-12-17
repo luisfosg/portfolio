d68e3df41f3227e440b0f2f02f692e4c0055f0db
luisfosg <luisfosgdev@gmail.com>
2023-12-17 14:47:30 -0500

    fix: quitando astro check (problemas con react class)

--- src/components/react/Quote.tsx
+++ src/components/react/Quote.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { getRandomQuote } from '@lib/quotes';
 
 const Quote: React.FC = () => {
@@ -6,8 +5,8 @@ const Quote: React.FC = () => {
 
   return (
     <>
-      <p className="text-white/70 text-center mt-24">{quote.text}</p>
-      <span className="w-2/3 text-center text-white/40 text-base leading-5">{quote.author}</span>
+      <p className={"text-white/70 text-center mt-24"}>{quote.text}</p>
+      <span className={"w-2/3 text-center text-white/40 text-base leading-5"}>{quote.author}</span>
     </>
   );
 };
--- src/components/react/Typing.tsx
+++ src/components/react/Typing.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 
 type Props = {
