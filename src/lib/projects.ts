bb6c5959207d0e2cb08d6e90f3868a4aed8337e0
luisfosg <luisfosgdev@gmail.com>
2023-12-16 21:32:16 -0500

    feat: agregando sección de proyectos

---
+++ src/lib/projects.ts
@@ -0,0 +1,31 @@
+type Project = {
+  name: string,
+  image: string
+};
+
+export const projects: Project[] = [
+  {
+    name: 'Proyecto Prueba',
+    image: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2672&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    name: 'Project 1',
+    image: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdlYnxlbnwwfHwwfHx8MA%3D%3D'
+  },
+  {
+    name: 'Otro nombre',
+    image: 'https://placedog.net/720'
+  },
+  {
+    name: 'Proyecto Prueba 1',
+    image: 'https://placedog.net/900'
+  },
+  {
+    name: 'Proyecto Prueba 2',
+    image: 'https://placedog.net/1000'
+  },
+  {
+    name: 'Proyecto Prueba 3',
+    image: 'https://placedog.net/500'
+  },
+]
