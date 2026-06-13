77979d3fd521f66d5beaf039ec69f7c6e8e2be19
Luis Osorio <luisfosgdev@gmail.com>
2026-06-13 09:40:54 -0500

    feat: migrate Tailwind to PostCSS setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
@@ -8,7 +7,6 @@ import spotlightjs from "@spotlightjs/astro";
 
 export default defineConfig({
   integrations: [
-    tailwind(),
     react(),
     sentry({
       sourceMapsUploadOptions: {
